a3499b974ac8d16fd19b0ea85c2b7f62e4b0ba53
Maíra de Oliveira <mairasetecores@gmail.com>
2017-05-17 18:05:55 -0300

    class: 58 - section: 9 - Firebase Client Setup

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
+
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+        apiKey: 'AIzaSyB5NLR29FvCgdc5TEZZtua4fs7im-TW97M',
+        authDomain: 'authentication-1c498.firebaseapp.com',
+        databaseURL: 'https://authentication-1c498.firebaseio.com',
+        projectId: 'authentication-1c498',
+        storageBucket: 'authentication-1c498.appspot.com',
+        messagingSenderId: '952425901003'
+    });
+  }
   render () {
     return (
       <View>
