19ec32826cfb020deb226168050c8263fd2354ff
Maíra de Oliveira <mairasetecores@gmail.com>
2017-05-17 09:57:30 -0300

    class: 47 - section: 8 - Making Content Scrollable

--- albums/index.android.js
+++ albums/index.android.js
@@ -2,12 +2,16 @@
 
 // Import a library to help crate a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
-import Header from './src/components/header';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header.js';
+import AlbumList from './src/components/AlbumList.js';
 
 // Create a Component
 const App = () => (
-    <Header />
+    <View style={{ flex: 1 }}>
+      <Header headerText={'Albums'} />
+      <AlbumList />
+    </View>
 );
 
 // Render it to device
