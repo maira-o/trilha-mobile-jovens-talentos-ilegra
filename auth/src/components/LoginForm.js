e14b601b15baec8dc5c663850ff349d162c850a7
Maíra de Oliveira <mairasetecores@gmail.com>
2017-05-18 11:12:36 -0300

    class: 66 - section: 9 - Wrapping up Inputs

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
-import { View, TextInput } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-  state = { text: '' };
+  state = { email: '' };
 
   render () {
     return (
       <Card>
         <CardSection>
-          <TextInput
-            value={this.state.text}
-            onChangeText={text => this.setState({ text })}
-            style={{ height: 20, width: 100 }}
+          <Input
+            placeholder="user@gmail.com"
+            label="Email"
+            value={this.state.email}
+            onChangeText={text => this.setState({ email })}
           />
         </CardSection>
+
         <CardSection />
 
         <CardSection>
