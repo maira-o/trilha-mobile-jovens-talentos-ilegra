7cc58b8508928d16d62275dc072cde948a7dc439
Maíra de Oliveira <mairasetecores@gmail.com>
2017-05-18 16:14:27 -0300

    class: 75 - section: 10 - More on Conditional Rendering

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 
 class App extends Component {
-  state = { loggedIn: false };
+  state = { loggedIn: null };
 
   componentWillMount() {
     firebase.initializeApp({
@@ -27,11 +27,21 @@ class App extends Component {
     });
   }
 
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return <Button>Log Out</Button>;
+      case false:
+        return <LoginForm />;
+      default: <Spinner size="large" />;
+    }
+  }
+
   render () {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
