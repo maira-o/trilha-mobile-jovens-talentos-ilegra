f70b816441bba0879c04dfe80d42a73f84d8a6cd
Maíra de Oliveira <mairasetecores@gmail.com>
2017-05-19 16:27:27 -0300

    class: 100 - section: 13 - Moving Logic Out of Components

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -6,9 +6,9 @@ import * as actions from '../actions';
 
 class ListItem extends Component {
   renderDescription() {
-    const { library, selectedLibraryId } = this.props;
+    const { library, expanded } = this.props;
 
-    if (library.id === selectedLibraryId) {
+    if (expanded) {
       return (
         <Text>{library.description}</Text>
       );
@@ -37,8 +37,10 @@ class ListItem extends Component {
   }
 }
 
-const mapStateToProps = state => {
-  return { selectedLibraryId: state.selectedLibraryId };
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryId === ownProps.library.id;
+
+  return { expanded };
 };
 
 const styles = {
