55870ae6c985a4bacb16f22865edfca84c14c900
Maíra de Oliveira <mairasetecores@gmail.com>
2017-05-23 10:45:29 -0300

    class: 108 - section: 15 - Handling Form Updates with Action Creators

--- manager/src/App.js
+++ manager/src/App.js
@@ -1,17 +1,29 @@
-import React, { component } from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: "AIzaSyDEKPMU9jGmTwLJwREjH_MzqWKPKwknHgc",
+      authDomain: "manager-b1638.firebaseapp.com",
+      databaseURL: "https://manager-b1638.firebaseio.com",
+      projectId: "manager-b1638",
+      storageBucket: "manager-b1638.appspot.com",
+      messagingSenderId: "37426985880"
+    };
+
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
-      <Provider store={createStore()}>
-        <View>
-          <Text>
-            Hello!
-          </Text>
-        </View>
+      <Provider store={createStore(reducers)}>
+        <LoginForm />
       </Provider>
     );
   }
